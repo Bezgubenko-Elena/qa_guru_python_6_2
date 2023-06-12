ae5ecf132182876b0a3b8d3db271031b6740baff
Bezgubenko Helen <protasova-ea@yandex.ru>
2023-06-12 13:37:34 +0300

    add fixture

--- test_search.py
+++ test_search.py
@@ -3,16 +3,18 @@ from selene import browser
 from selene import be, have
 
 
-
-
-
-
-def test_search_success():
+@pytest.fixture()
+def browser_open():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.open('https://google.com')
+
+
+def test_search_success(browser_open):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_search_fail():
-    browser.open('https://google.com')
+
+def test_search_nothing(browser_open):
     browser.element('[name="q"]').should(be.blank).type('hgyfutydkfchgvjbhkj').press_enter()
     browser.element('[id="topstuff"]').should(have.text('По запросу hgyfutydkfchgvjbhkj ничего не найдено.'))
